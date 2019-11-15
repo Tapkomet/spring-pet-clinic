9c227e0eaf5a38dc9f36f451744de4f6fe22c5bf
Tapkomet <romakobzar@ukr.net>
2019-11-16 00:21:47 +0200

    Adding index page and controller. Closes #8.

---
+++ pet-clinic-web/src/main/java/tapkomet/springframework/springpetclinic/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package tapkomet.springframework.springpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Tapkomet on 11/16/2019
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index() {
+        return "index";
+    }
+}
