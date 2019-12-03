dc131b38bbf65f288c00bdb3bd90761b922ff0d3
Tapkomet <romakobzar@ukr.net>
2019-12-03 02:01:03 +0200

    Added owners list and updated controller. Closes #16

--- pet-clinic-web/src/main/java/tapkomet/springframework/springpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/tapkomet/springframework/springpetclinic/controllers/OwnerController.java
@@ -1,7 +1,9 @@
 package tapkomet.springframework.springpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import tapkomet.springframework.springpetclinic.services.OwnerService;
 
 /**
  * Created by Tapkomet on 11/18/2019
@@ -10,8 +12,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owners/index";
     }
 }
