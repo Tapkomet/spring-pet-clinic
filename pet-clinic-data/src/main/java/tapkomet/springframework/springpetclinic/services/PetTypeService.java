e7f6544b8a380bb1fb797190294db6e46648aebe
Tapkomet <romakobzar@ukr.net>
2020-01-29 21:48:34 +0200

    Added PetTypeService. Closes #29

---
+++ pet-clinic-data/src/main/java/tapkomet/springframework/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package tapkomet.springframework.springpetclinic.services;
+
+import tapkomet.springframework.springpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>   {
+}
