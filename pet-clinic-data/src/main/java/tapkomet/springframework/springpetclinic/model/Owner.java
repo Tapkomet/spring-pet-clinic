fe6bcc19ffcd5c012404fbe22c74696e99bcce0f
Tapkomet <romakobzar@ukr.net>
2020-01-29 19:45:41 +0200

    Added Visit Entity, updated Owner with Pet relationship. Closes #26

--- pet-clinic-data/src/main/java/tapkomet/springframework/springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/tapkomet/springframework/springpetclinic/model/Owner.java
@@ -1,7 +1,19 @@
 package tapkomet.springframework.springpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by Tapkomet on 10/20/2019
  */
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
