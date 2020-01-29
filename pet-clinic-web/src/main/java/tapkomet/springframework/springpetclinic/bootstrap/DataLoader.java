ce6123c31e5ddc6596a616c7d6219408d9e2ef92
Tapkomet <romakobzar@ukr.net>
2020-01-29 21:56:17 +0200

    Added PetTypes to bootstrap DataLoader. Closes #32

--- pet-clinic-web/src/main/java/tapkomet/springframework/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/tapkomet/springframework/springpetclinic/bootstrap/DataLoader.java
@@ -3,8 +3,10 @@ package tapkomet.springframework.springpetclinic.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import tapkomet.springframework.springpetclinic.model.Owner;
+import tapkomet.springframework.springpetclinic.model.PetType;
 import tapkomet.springframework.springpetclinic.model.Vet;
 import tapkomet.springframework.springpetclinic.services.OwnerService;
+import tapkomet.springframework.springpetclinic.services.PetTypeService;
 import tapkomet.springframework.springpetclinic.services.VetService;
 
 /**
@@ -15,14 +17,27 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
+
     }
 
     @Override
     public void run(String... args) throws Exception {
+        PetType cat = new PetType();
+        cat.setName("cat");
+
+        PetType savedCatPetType = petTypeService.save(cat);
+
+        PetType dog = new PetType();
+        dog.setName("dog");
+
+        PetType savedDogPetType = petTypeService.save(dog);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
