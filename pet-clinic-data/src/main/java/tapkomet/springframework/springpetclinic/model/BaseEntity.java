22fd706fd62c6bd1203c67619a3efb25bc99efcd
Tapkomet <romakobzar@ukr.net>
2020-02-04 21:09:34 +0200

    Updated base entity with JPA annotations. Closes #35

--- pet-clinic-data/src/main/java/tapkomet/springframework/springpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/tapkomet/springframework/springpetclinic/model/BaseEntity.java
@@ -1,12 +1,19 @@
 package tapkomet.springframework.springpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  * Created by Tapkomet on 11/12/2019
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
