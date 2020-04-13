ef479c11ed37df6f2a9464a38f86fbecbda71571
Tapkomet <romakobzar@ukr.net>
2020-04-13 16:53:50 +0300

    Created Visit Controller test. Added a converter from string to date in Visit Controller. Closes #76

--- pet-clinic-web/src/main/java/tapkomet/springframework/springpetclinic/controllers/VisitController.java
+++ pet-clinic-web/src/main/java/tapkomet/springframework/springpetclinic/controllers/VisitController.java
@@ -11,6 +11,8 @@ import tapkomet.springframework.springpetclinic.services.PetService;
 import tapkomet.springframework.springpetclinic.services.VisitService;
 
 import javax.validation.Valid;
+import java.beans.PropertyEditorSupport;
+import java.time.LocalDate;
 
 /**
  * Created by Tapkomet on 3/5/2020
@@ -32,12 +34,20 @@ class VisitController {
     @InitBinder
     public void setAllowedFields(WebDataBinder dataBinder) {
         dataBinder.setDisallowedFields("id");
+
+        dataBinder.registerCustomEditor(LocalDate.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) throws IllegalArgumentException {
+                setValue(LocalDate.parse(text));
+            }
+        });
     }
 
     /**
      * Called before each and every @RequestMapping annotated method. 2 goals: - Make sure
      * we always have fresh data - Since we do not use the session scope, make sure that
      * Pet object always has an id (Even though id is not part of the form fields)
+     *
      * @param petId
      * @return Pet
      */
@@ -62,8 +72,7 @@ class VisitController {
     public String processNewVisitForm(@Valid Visit visit, BindingResult result) {
         if (result.hasErrors()) {
             return "pets/createOrUpdateVisitForm";
-        }
-        else {
+        } else {
             visitService.save(visit);
             return "redirect:/owners/{ownerId}";
         }
